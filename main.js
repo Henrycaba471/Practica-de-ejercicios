eaf01f1878a4b1b818c01a68e62e195491d58e78
Henry <henrycaba471@gamil.com>
2021-04-10 20:35:00 -0500

    Practicando

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ let texto = '';
 const contarCaracteres = (texto) => {
     (!texto)
         ? console.warn('!No hay texto ingresado')
-        : console.info(`El texto |►${texto}◄| contiene ${texto.length} caracteres`);
+        : console.info(`1. El texto |►${texto}◄| contiene ${texto.length} caracteres`);
 }
 contarCaracteres('Contando caracteres');
 
@@ -18,7 +18,7 @@ const recortarTexto = (texto1, longitud) => {
     if (longitud === undefined || typeof longitud !== 'number') {
         return console.warn('No has ingresado el recorte o el valor ingresado no es valido');
     }
-    return console.info(`El texto a recortar es |►${texto1}◄| y el recorte es |►${longitud}◄| Nuevo texto ♦${texto1.slice(0, longitud)}♦`);
+    return console.info(`2. El texto a recortar es |►${texto1}◄| y el recorte es |►${longitud}◄| Nuevo texto ♦${texto1.slice(0, longitud)}♦`);
 }
 recortarTexto('Recortando el texto', 10);
 
@@ -32,7 +32,7 @@ const stringToArray = (texto2, separador) => {
     if (separador === undefined) {
         return console.warn('No has ingresado el separador o el valor ingresado no es valido');
     }
-    return console.info(`El texto a convertir es |►${texto2}◄| quedaria`, texto2.split(separador));
+    return console.info(`3. El texto a convertir es |►${texto2}◄| quedaria`, texto2.split(separador));
 }
 
 stringToArray('Convirtiendo una cadena en arreglo', ' ');
@@ -47,7 +47,7 @@ const repetirTexto = (texto3,repetidor) => {
     if (repetidor === undefined || typeof repetidor !== 'number') {
         return console.warn('No has ingresado las veces a repetir o el valor ingresado no es valido');
     }
-    console.info(`El texto a repetir es |►${texto3}◄| las veces a repetir son |►${repetidor}◄|`);
+    console.info(`4. El texto a repetir es |►${texto3}◄| las veces a repetir son |►${repetidor}◄|`);
     for (let i = 1; i <= repetidor; i++) {
         console.info(texto3, i);
     }
@@ -63,9 +63,37 @@ const invertirTexto = (texto4 = '') => {
     if (typeof texto4 !== 'string') {
         return console.warn('Por el momento solo estamos invirtiendo texto');
     }
-    return console.info(texto4.split('').reverse().join(''));
+    return console.info('5.',texto4.split('').reverse().join(''));
 
 }
 invertirTexto('invirteindo texto');
 
 //6) Programa una función para contar el número de veces que se repite una palabra en un texto largo, pe. miFuncion("hola mundo adios mundo", "mundo") devolverá 2.
+const buscarPalabra = (texto5 = '', palBuscada = '') => {
+    if (!texto5) {
+        return console.warn('No has ingresado ningun texto');
+    }
+    if (!palBuscada) {
+        return console.warn('No has ingresado la palabra que deses buscar');
+    }
+    if (typeof palBuscada !== 'string') {
+        return console.warn('Por ahora solo se permite texto');
+    }
+    let i = 0,
+        contador = 0;
+
+    while (i !== -1) {
+        i = texto5.indexOf(palBuscada,i);
+        if (i !== -1) {
+            i++,
+            contador++;
+        }
+    }
+    return console.info(`6. La palabra |►${palBuscada}◄| se repite ${contador} veces en el texto`);
+}
+buscarPalabra('Lorem ipsum, dolor sid amet consectetur adipi sid cing sid. Iure error sed voluptas soluta unde id atque maiores accusamus cum sid ctio. Exercitationem quod optio iusto eligendi. Consectetur similique non totam saepe', 'sid');
+
+//7) Programa una función que valide si una palabra o frase dada, es un palíndromo (que se lee igual en un sentido que en otro), pe. mifuncion("Salas") devolverá true.
+const palindromo = (texto6 = '') =>{
+
+}
